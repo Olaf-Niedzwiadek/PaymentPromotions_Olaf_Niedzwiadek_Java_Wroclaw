7d762bac9b64f618914fcec638c3627efed408dd
Olaf <olaffnn@gmail.com>
2025-05-09 22:58:25 +0200

    initial commit

---
+++ src/main/java/com/example/paymentpromotions_olaf_niedzwiadek_java_wroclaw/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.paymentpromotions_olaf_niedzwiadek_java_wroclaw;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
